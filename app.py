3840a19065fd729554f715e8e9057de93e5e197e
Kanjana <kanjanavelusamy1483@gmail.com>
2026-01-30 08:28:27 +0530

    Adding voltage limit

--- app.py
+++ app.py
@@ -118,16 +118,21 @@ def latest_data():
 @app.route("/api/control",
 methods=["POST"])
 def control_relay():
-    global relay_status
+    global relay_status, MAX_VOLTAGE
     command = request.json.get("status")
 
     if command == "OFF":
         relay_status = "OFF"
+        MAX_VOLTAGE = 0   # voltage becomes 0 when OFF
 
     elif command == "ON" and fault_status == "NORMAL":
-        relay_statsu = "ON"
+        relay_status = "ON"
+        MAX_VOLTAGE = 250   # voltage limit normal when ON
 
-    return jsonify({"relay": relay_status})
+    return jsonify({
+        "relay": relay_status,
+        "MAX_VOLTAGE": MAX_VOLTAGE
+    })
 
 #============================ RUN SERVER ==========================
 if __name__ == "__main__":
